4714ae1a60f3785409c8c3fb60054d42aed563d8
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2012-09-18 10:28:30 +0530

    Improved lancher infrastructure

--- lib/exec/firstrun.bash
+++ lib/exec/firstrun.bash
@@ -5,12 +5,12 @@ uniqrun_run || exit 0
 
 trap uniqrun_exit EXIT
 
-
+sleep 5
 if [ -f  $import_datapath/firstrun ];then
 for i in $(cat $import_datapath/firstrun);
 do
 (
-    
+    sleep 1
     launcher.bash $i &
 )
 done
--- lib/exec/pppguard.bash
+++ lib/exec/pppguard.bash
@@ -4,14 +4,15 @@ import network
 import google
 import uniqrun
 set -x
-exec 2>$BGIT_BASEDIR/lib/vardata/log/pppguard.log
+exec 2>&1
 uniqrun_global=yes
 uniqrun_run ||exit
 
 trap uniqrun_exit EXIT
 while true;do
     if network_isppp; then
-	  google_ping >/dev/null 2>&1 
+       echo "#Date:$(date)">>$BGIT_BASEDIR/lib/vardata/log/pppguard.log      
+	  google_ping >>$BGIT_BASEDIR/lib/vardata/log/pppguard.log
     sleep 40
     else
     sleep 100
