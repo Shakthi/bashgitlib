b7498fe43098b68b87cf8edbe2f90f7ecd11c73a
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2013-02-18 13:45:57 +0530

    Fixed not exit lancher cofig

--- lib/exec/launcherlauncher.bash
+++ lib/exec/launcherlauncher.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
-read cmd <$launcher_datapath/launcherlauncher
-if [ ! -z $cmd ];then 
-cmd=$(which $cmd)
-setsid $cmd >/dev/null 2>/dev/null </dev/null &
+
+if [ -r $launcher_datapath/launcherlauncher ];then
+    read cmd <$launcher_datapath/launcherlauncher
+    if [ ! -z $cmd ];then 
+    cmd=$(which $cmd)
+    setsid $cmd >/dev/null 2>/dev/null </dev/null &
+    fi
 fi
